8850a81625000a489e84388a9b51d656e94f9fa9
Stephen Yacas <syacas123@gmail.com>
2021-04-01 20:27:15 -0400

    variables added to js

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+let checkBoxSeriesX = $("#checkbox-seriesX");
+let checkBoxSeriesS = $("#checkbox-seriesS");
+
+let zipInput = $("#zip-input");
+let radiusInput = $("#radius-input");
+
+let searchBtn =$("#search-btn");
