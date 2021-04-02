36a1982a2519aa5c316cc4e6427107fc8fb46129
Stephen Yacas <syacas123@gmail.com>
2021-04-01 21:23:50 -0400

    edits to js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,11 @@ let zipInput = $("#zip-input");
 let radiusInput = $("#radius-input");
 
 let searchBtn =$("#search-btn");
+
+searchBtn.on("click", function (){
+    console.log("lets gooooo");
+})
+
+$("#search-btn").on("click", function () {
+    console.log("get it, erick?")
+});
