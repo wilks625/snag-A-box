68674e1f5219cba8e1b128f0c6731243246e3835
Stephen Yacas <syacas123@gmail.com>
2021-04-03 13:48:29 -0400

    added accessory css files as well as edits to js file for api functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,10 +6,12 @@ let radiusInput = $("#radius-input");
 
 let searchBtn =$("#search-btn");
 
-searchBtn.on("click", function (){
-    console.log("lets gooooo");
-})
+// let bestBuy = $("#bestbuy-instore-avail")
+let bestBuy = document.getElementById("bestbuy-instore-avail")
 
+// searchBtn.on("click", function (){
+//     console.log("lets gooooo");
+// })
 
 
 
@@ -19,8 +21,8 @@ searchBtn.on("click", function (){
 
 
 function getApi() {
-    var requestUrl = 'https://api.github.com/users?per_page=5';
-  
+    var requestUrl = 'https://api.bestbuy.com/v1/products((search=6428324))?apiKey=XjL9LhCyOmECuslGN2ftjQG9&pageSize=21&format=json';
+    console.log(requestUrl);
     fetch(requestUrl)
       .then(function (response) {
         return response.json();
@@ -28,23 +30,28 @@ function getApi() {
       .then(function (data) {
         //Using console.log to examine the data
         console.log(data);
+        
         for (var i = 0; i < data.length; i++) {
           //Creating a h3 element and a p element
-          var userName = document.createElement('h3');
-          var userUrl = document.createElement('p');
+          // var userName = document.createElement('h3');
+          
+          var instoreAvail = document.createElement("p")
+          console.log("lets go?")
+
   
           //Setting the text of the h3 element and p element.
-          userName.textContent = data[i].login;
-          userUrl.textContent = data[i].url;
+          instoreAvail.textContent = data[i].inStoreAvailability;
+          
   
           //Appending the dynamically generated html to the div associated with the id="users"
           //Append will attach the element as the bottom most child.
-          usersContainer.append(userName);
-          usersContainer.append(userUrl);
+          bestBuy.append(instoreAvail);
+          
+         
         }
       });
   }
-
+  searchBtn.on('click', getApi);
 
 // $("#search-btn").on("click", function () {
 //     console.log("get it, erick?")
